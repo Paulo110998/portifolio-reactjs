22930676904a56b6e6cc1c601d1584535c149e78
Paulo Alves <107944987+Paulo110998@users.noreply.github.com>
2023-12-30 15:22:06 -0300

    up

--- src/componentes/Projetos.jsx
+++ src/componentes/Projetos.jsx
@@ -25,6 +25,8 @@ import {GoProjectSymlink} from 'react-icons/go'
 
 import ScrollReveal from "scrollreveal";
 
+import { PiCursorClickBold } from "react-icons/pi";
+
 ScrollReveal({ reset: true });
 
 ScrollReveal().reveal('.projetos', { delay: 370 });
@@ -35,40 +37,38 @@ function Projetos(){
         <div className="projetos">
             <h1 className="h1-projetos">Projects <GoProjectSymlink/></h1>
             <br />
-          <a target="_blank" href="https://brasilportugalinvest360.com/"><ul className="card-list">
-          <li className="list-cards" >
-            <div class="card">
-              <br />
-              <img  className="projetos-img" src={Invest} alt="" />
-              <p className="descriçao1" ><b></b></p>
-              <br />
-              <p className="descriçao2"><b>O Invest360 é uma aplicação web com foco em investimentos no mercado luso-brasileiro.
-               Desenvolvida em python com django, e publicada em cloud na DigitalOcean.</b></p>
-              <br />
-              <p className="descriçao5">Stacks:</p>
-              <p className="descriçao6"><SiDjango/>  <SiPython/>  <CgDigitalocean/>  <AiFillGithub/>  <BsGit/>  <ImHtmlFive/> <ImCss3/> <IoLogoJavascript/> <BsFillBootstrapFill/>  <GrMysql/> </p>
-              <br />
-              <a href="https://brasilportugalinvest360.com/" target="_blank">Acesse: Invest360º <CgBrowser/></a>
-            </div>
-          </li>
-        </ul></a>
-
-        <a href="https://github.com/Paulo110998/portifolio-reactjs.git" target="_blank"><ul className="card-list">
+         <a href="https://github.com/Paulo110998/portifolio-reactjs.git" target="_blank"><ul className="card-list">
           <li className="list-cards" >
             <div class="card">
               <br />
               <img src={React} height={5} alt="" />
               <br />
               <br />
-              <p className="descriçao1" ><b>Esse portifólio é desenvolvido em React e publicado na Vercel. Acesse o repositório demonstrativo no Github. </b></p>
+              <p className="descriçao1" >Esse portifólio é desenvolvido em React e publicado na Vercel. Acesse o repositório demonstrativo no Github.</p>
               <p className="descriçao2" ></p>
-              <p className="descriçao3" ></p>
-              <p className="descriçao4" ></p>
+              
               <br />
               <p className="descriçao5">Stacks:</p>
-              <p className="descriçao6" ><FaReact/> <RxVercelLogo/> </p>
+              <p className="descriçao6" ><IoLogoJavascript/> <FaReact/> <RxVercelLogo/> </p>
               <br />
-              <a href="https://github.com/Paulo110998/portifolio-reactjs.git" target="_blank">Acesse: Repositório  <AiFillGithub/></a>
+              <a href="https://github.com/Paulo110998/portifolio-reactjs.git" target="_blank"></a>
+            </div>
+          </li>
+        </ul></a>
+
+        <a target="_blank" href="https://brasilportugalinvest360.com/"><ul className="card-list">
+          <li className="list-cards" >
+            <div class="card">
+              <br />
+              <img  src={Django} alt="" />
+              <p className="descriçao1" ><b></b></p>
+              <p className="descriçao2">O Invest360 é uma aplicação web com foco em investimentos no mercado luso-brasileiro.
+               Desenvolvida em python com django, e publicada em cloud na DigitalOcean.</p>
+              <br />
+              <p className="descriçao5">Stacks:</p>
+              <p className="descriçao6"><SiDjango/>  <SiPython/>  <CgDigitalocean/>  <AiFillGithub/>  <BsGit/>  <ImHtmlFive/> <ImCss3/> <IoLogoJavascript/> <BsFillBootstrapFill/>  <GrMysql/> </p>
+              <br />
+              <a href="https://brasilportugalinvest360.com/" target="_blank"></a>
             </div>
           </li>
         </ul></a>
@@ -80,15 +80,14 @@ function Projetos(){
               <img src={Django} alt="" />
               <br />
            
-              <p className="descriçao1" ><b>Your Projects é uma aplicação web demonstrativa, desenvolvida em python com django. Acesse para visualizar a arquitetura MTV do django.</b></p>
+              <p className="descriçao1" >Your Projects é uma aplicação web demonstrativa, desenvolvida em python com django. Acesse para visualizar a arquitetura MTV do django.</p>
               <p className="descriçao2" ></p>
-              <p className="descriçao3" ></p>
-              <p className="descriçao4" ></p>
+              
               <br />
               <p className="descriçao5">Stacks:</p>
               <p className="descriçao6" ><SiDjango/>  <SiPython/>  <CgDigitalocean/>  <AiFillGithub/>  <BsGit/>  <ImHtmlFive/> <ImCss3/> <IoLogoJavascript/> <BsFillBootstrapFill/>  <GrMysql/></p>
               <br />
-              <a href="https://clownfish-app-jitiq.ondigitalocean.app/" target="_blank">Acesse: Your Projects <CgBrowser/></a>
+              <a href="https://clownfish-app-jitiq.ondigitalocean.app/" target="_blank"></a>
             </div>
           </li>
         </ul></a>
@@ -96,19 +95,18 @@ function Projetos(){
         <a href="https://github.com/Paulo110998/Music_api_consumption.git" target="_blank"><ul className="card-list">
           <li className="list-cards" >
             <div class="card">
-              <br />
+              
               <img src={Dotnet} alt="" />
-              <br />
-           
+                       
               <p className="descriçao1" ><b>Music API consumption</b></p>
-              <p className="descriçao2" ></p>
-              <p className="descriçao3" ><b>O projeto exibe a desserialização e serialização de dados de uma API de música, o uso do HttpClient (.NET) e a criação de arquivos em C#.</b></p>
-              <p className="descriçao4" ></p>
+              <p className="descriçao3" >O projeto exibe a desserialização e serialização de dados de uma API de música, o uso do HttpClient (.NET) e a criação de arquivos em C#.</p>
+              
               <br />
+             
               <p className="descriçao5">Stacks:</p>
               <p className="descriçao6" ><TbBrandCSharp/> <DiDotnet /> <AiFillGithub/>  <BsGit/> </p>
               <br />
-              <a href="https://github.com/Paulo110998/Music_api_consumption.git" target="_blank">Acesse: Repositório <AiFillGithub/></a>
+              <a href="https://github.com/Paulo110998/Music_api_consumption.git" target="_blank"></a>
             </div>
           </li>
         </ul></a>
@@ -118,14 +116,12 @@ function Projetos(){
             <div class="card">
               <img src={Dotnet} alt="" />
               <p className="descriçao1" ><b>Web API Streaming</b></p>
-              <p className="descriçao2" ></p>
-              <p className="descriçao3" ><b>O projeto é um modelo prático de API Restful MVC. Temos a utilização do EntityFramework com Mysql, e também o AutoMapper com mapeamento de DTOs.</b></p>
-              <p className="descriçao4" ></p>
+              <p className="descriçao3" >Um modelo prático de API Rest MVC. Com a utilização do EntityFramework e Mysql, relacionamento de entidades e consultas com LINQ, e também o mapeamento de DTOs.</p>
               <br />
               <p className="descriçao5">Stacks:</p>
               <p className="descriçao6" ><TbBrandCSharp/> <DiDotnet /> <AiFillGithub/>  <BsGit/> <GrMysql/> </p>
               <br />
-              <a href="https://github.com/Paulo110998/Music_api_consumption.git" target="_blank">Acesse: Repositório <AiFillGithub/></a>
+              <a href="https://github.com/Paulo110998/Music_api_consumption.git" target="_blank"></a>
             </div>
           </li>
         </ul></a>
@@ -139,15 +135,13 @@ function Projetos(){
               <h3>Jadson Bittencourt</h3>
               <br />
            
-              <p className="descriçao1" ><b>Site desenvolvido em Html5, Css3 e Javascript e publicado no bohr.io, para o palestrante Jadson Bittencourt.</b></p>
+              <p className="descriçao1" >Site desenvolvido em Html5, Css3 e Javascript e publicado no bohr.io, para o palestrante Jadson Bittencourt.</p>
               <p className="descriçao2" ></p>
-              <p className="descriçao3" ></p>
-              <p className="descriçao4" ></p>
               <br />
               <p className="descriçao5">Stacks:</p>
               <p className="descriçao6" > <AiFillGithub/>  <BsGit/>  <ImHtmlFive/> <ImCss3/> <IoLogoJavascript/></p>
               <br />
-              <a href="https://jadsonbitencourtt.com.br/" target="_blank">Acesse: jadsonbitencourtt.com.br <CgBrowser/></a>
+              <a href="https://jadsonbitencourtt.com.br/"  target="_blank"></a>
             </div>
           </li>
         </ul></a>
--- src/componentes/contato.jsx
+++ src/componentes/contato.jsx
@@ -3,6 +3,7 @@ import {BsWhatsapp} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
 import {BsLinkedin} from 'react-icons/bs'
 import Lets from './img/lets_code.gif'
+import Click from './img/click.gif'
 
 function Contato(){
     return(
@@ -11,6 +12,9 @@ function Contato(){
             <h1 className="h1-contato" >Contact-Us <PiContactlessPaymentFill/></h1>
             <br />
             <h1 className='redes-sociais'><a href="https://api.whatsapp.com/send?phone=5582993112813"><BsWhatsapp/></a>  <a href="https://www.instagram.com/paulo_works/?igshid=NTdlMDg3MTY%3D"><BsInstagram/></a>  <a href="https://www.linkedin.com/in/paulo-alves-76a9b6205/"><BsLinkedin/></a></h1>
+            </div>
+            <div>
+                <img className='clickme' src={Click} alt="" />
             </div>
             <div>
                 <img className='lets_code'  src={Lets} alt="" />
